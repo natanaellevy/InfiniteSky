8293ac255dceb8dbedd8e91a019dc04574d84342
natanaellevy <devnatanaellevy@gmail.com>
2026-03-22 16:36:31 -0300

    V3

--- code/Const.py
+++ code/Const.py
@@ -1,6 +1,14 @@
 # C
 COLOR_ORANGE = (255, 128, 0)
+COLOR_YELLOW = (255, 255, 0)
 COLOR_WHITE = (255, 255, 255)
+
+# E
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+}
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
